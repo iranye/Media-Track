01f0b0b8e3478c33f68174e8c69f0065ef477847
Ira Nye <iranye@gmail.com>
2018-05-05 13:59:39 -0700

    updates

--- BackupToNAS.bat
+++ BackupToNAS.bat
@@ -1,1 +1,4 @@
-robocopy I:\MediaTracker \\192.168.1.35\Public\Shared\MediaTracker /e /s /r:5 /w:5 /xd MakeMKV-Movies /xd RecordedTV
+REM robocopy I:\MediaTracker \\192.168.1.35\Public\Shared\MediaTracker /e /s /r:5 /w:5 /xd MakeMKV-Movies /xd RecordedTV
+
+robocopy C:\MediaTracker \\synnas\Shared\MediaTracker /e /s /r:5 /w:5 /xd ToBurn /xd .git
+
