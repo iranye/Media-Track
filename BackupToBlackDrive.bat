75d0963ec95c8ef45bb1419f1892dcb514a6e901
Ira Nye <iranye@gmail.com>
2018-05-26 16:43:02 -0700

    clean up

--- BackupToBlackDrive.bat
+++
@@ -1,4 +0,0 @@
-REM robocopy I:\MediaTracker \\192.168.1.35\Public\Shared\MediaTracker /e /s /r:5 /w:5 /xd MakeMKV-Movies /xd RecordedTV
-
-robocopy C:\MediaTracker\CloudStation G:\MediaTracker /mir /tee /v /r:5 /w:5 /xd ToBurn /xd .git /xd .SynologyWorkingDirectory /log+:C:\MediaTracker\robolog_042018.log
-
--- BackupToNAS.bat
+++
@@ -1,4 +0,0 @@
-REM robocopy I:\MediaTracker \\192.168.1.35\Public\Shared\MediaTracker /e /s /r:5 /w:5 /xd MakeMKV-Movies /xd RecordedTV
-
-robocopy C:\MediaTracker \\synnas\Shared\MediaTracker /e /s /r:5 /w:5 /xd ToBurn /xd .git
-
