04884828ec07e5af7f9444ae725167ef5e704649
iranye <iranye@gmail.com>
2018-12-07 16:36:37 -0800

    updates

--- Music/__FixStuff__.cmd
+++ Music/__FixStuff__.cmd
@@ -13,3 +13,6 @@ Copy /y "E:\Media\Music\MP3s\Frankie Valli & the Four Seasons\The Four Seasons_0
 md5sum "E:\Media\Music\MP3s\Frankie Valli & the Four Seasons\The Four Seasons_05_Dawn (Go Away).mp3" & md5sum "E:\Media\Music\SimpleDriveMusic\0_Compilations\Billboard Hits 1946-2004\1964\1964-025 Four Seasons - Dawn (Go Away).mp3"
 md5sum "E:\Media\Music\MP3s\Frankie Valli & the Four Seasons\The Four Seasons_06_Ronnie.mp3" & md5sum "E:\Media\Music\SimpleDriveMusic\0_Compilations\Billboard Hits 1946-2004\1964\1964-075 Four Seasons - Ronnie.mp3"
 md5sum "E:\Media\Music\MP3s\Frankie Valli & the Four Seasons\The Four Seasons_07_Rag Doll.mp3" & md5sum "E:\Media\Music\SimpleDriveMusic\0_Compilations\Billboard Hits 1946-2004\1964\1964-024 Four Seasons - Rag Doll.mp3"
+
+PAUSE
+copy /y "E:\Media\Music\SimpleDriveMusic\0_Personal Collections\0_Samantha's music\65) the doors - greatest hits\The Doors - Riders On The Storm.mp3" "E:\Media\Music\SimpleDriveMusic\0_Compilations\Billboard Hits 1946-2004\1971\1971-099 Doors - Riders on the Storm.mp3"
