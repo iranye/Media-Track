6dca72dfea101769f79533ec19d1590ffc72dc79
ira <ranye@gmail.com>
2019-03-25 21:53:49 -0700

    updates

--- Magic/Exports/_cleanup.cmd
+++ Magic/Exports/_cleanup.cmd
@@ -12,12 +12,15 @@ move /y "Dragons.dek" %dip%
 move /y "ElementsTokens.dek" %dip%
 move /y "EliteIsochron.dek" %dip%
 move /y "FourPowerMobs_Arties.dek" %dip%
+move /y "Godsire.dek" %dip%
 move /y "GreenBlackApex.dek" %dip%
 move /y "GreenBlackApexFights.dek" %dip%
 move /y "GreenBlackEnchants.dek" %dip%
 move /y "Haunted.dek" %dip%
 move /y "Horrors_LivingLore.dek" %dip%
 move /y "Horrors_White.dek" %dip%
+move /y "KittyCat.dek" %dip%
+move /y "LifePay.dek" %dip%
 move /y "LandDestruct.dek" %dip%
 move /y "Legendaries.dek" %dip%
 move /y "LivingLore.dek" %dip%
