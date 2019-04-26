cba1653d6f65597c8fc84bca9aa27799dcb9afbb
ira nye <ira nye@nvoicepay.com>
2019-04-26 13:43:18 -0700

    updates

--- Magic/Exports/_cleanup.cmd
+++ Magic/Exports/_cleanup.cmd
@@ -3,7 +3,6 @@ set dip=C:\Temp\DecksInProgress
 mkdir %dip%
 
 move /y "Activation.dek" %dip%
-move /y "Bears.dek" %dip%
 move /y "BigSpells.dek" %dip%
 move /y "BlackWhiteSac.dek" %dip%
 move /y "BlueBlackMacar.dek" %dip%
@@ -11,21 +10,16 @@ move /y "CountersInspired.dek" %dip%
 move /y "Dragons.dek" %dip%
 move /y "ElementsTokens.dek" %dip%
 move /y "EliteIsochron.dek" %dip%
-move /y "FourPowerMobs_Arties.dek" %dip%
+move /y "Godsire.dek" %dip%
 move /y "Haunted.dek" %dip%
-move /y "Horrors_LivingLore.dek" %dip%
 move /y "Horrors_White.dek" %dip%
-move /y "KittyCat.dek" %dip%
-move /y "LifePay.dek" %dip%
 move /y "LandDestruct.dek" %dip%
 move /y "Legendaries.dek" %dip%
 move /y "LivingLore.dek" %dip%
-move /y "MarduMidRange.dek" %dip%
 move /y "SimicBlueGreen.dek" %dip%
 move /y "Specters.dek" %dip%
 move /y "SummonDemon.dek" %dip%
 move /y "TargetsForCounters.dek" %dip%
-move /y "Tokens_Red.dek" %dip%
 move /y "WasteNot.dek" %dip%
 move /y "YACountersDeck.dek" %dip%
 
