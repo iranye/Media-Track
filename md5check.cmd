5f5320d288e6c6457643c64f2167704e3b2851a0
iranye <iranye@gmail.com>
2020-09-20 22:41:54 -0700

    udpates

--- md5check.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-set output=I:\MediaTracker\output.md5
-md5sum "I:\MediaTracker\MakeMKV-Movies\_Family_Flat\Charlie and the Chocolate Factory_KATUDT_2011_12_11_21_00_01.wtv" >> %output%
-
-md5sum "E:\_Overflow\RecordedTV\Movies\Charlie and the Chocolate Factory_KATUDT_2011_12_11_21_00_01.wtv" >> %output%
